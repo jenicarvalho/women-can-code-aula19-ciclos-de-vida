7dff6a35f1cc7e908be741591f0740f92e97ed11
Jeniffer Carvalho <jenicarvalho@live.com>
2020-03-30 18:49:40 -0300

    ciclos de vida

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DidMount from './DidMount'
+import DidUpdate from './DidUpdate'
+import DidUpdate2 from './DidUpdate2'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DidMount />
+    // <DidUpdate />
+    // <DidUpdate2 />
   );
 }
-
-export default App;
